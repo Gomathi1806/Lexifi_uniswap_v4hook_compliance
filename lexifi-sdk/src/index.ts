6bc15d64498681503f85fa784c1a491aaa025af1
gomathi <tgomathi1806@gmail.com>
2026-08-06 13:05:02 +0530

    fix: bundle v3 SDK inside repo for Vercel builds
    Replace old v1 SDK with correct v3 SDK (addresses, ABIs, types) and
    change package.json from file:../lexifi-sdk to file:./lexifi-sdk so
    the Vercel build can resolve @lexifi/sdk without the parent directory.

--- lexifi-sdk/src/index.ts
+++ lexifi-sdk/src/index.ts
@@ -1,3 +1,32 @@
-export * from "./abis";
-export * from "./deployments";
-export * from "./types";
+export {
+  LexifiHookAbi,
+  ThresholdPolicyAbi,
+  RegionalPolicyAbi,
+  InstitutionalPolicyAbi,
+  VerificationProviderAbi,
+  SelfAttestationProviderAbi,
+  EventTopics,
+} from "./abis.js";
+
+export {
+  base,
+  baseSepolia,
+  getDeployment,
+  type LexifiDeployment,
+} from "./addresses.js";
+
+export {
+  AccessLevel,
+  AccessLevelLabels,
+  Operation,
+  type PoolKey,
+  type ComplianceResult,
+  type PoolInfo,
+  type VerificationResult,
+  type ThresholdConfig,
+  type RegionalConfig,
+  type InstitutionalConfig,
+  type AuditRecord,
+} from "./types.js";
+
+export { fetchAuditTrail, type FetchAuditOptions } from "./audit.js";
